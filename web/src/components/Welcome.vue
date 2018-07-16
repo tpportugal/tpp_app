0ac2ad8f50b1e4b79cf6eb0becc7f2b2f25dae2b
André Glatzl <andre@tpp.pt>
2018-07-16 21:55:14 +0200

    Initial commit - web project

---
+++ web/src/components/Welcome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="welcome">
+    <nav>
+      Hello
+    </nav>
+    <h2>{{ msg }}</h2>
+    <div class="test">
+      <p>BIG AND SMALL</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+#welcome {
+  width: 100%;
+  height: 100vh;
+  nav {
+    background-color: #0391ff;
+    color: #FFF;
+    font-weight: lighter;
+  }
+  h2 {
+    color: blue;
+  }
+  .test {
+    width: 300px;
+    background-color: black;
+    p {
+      color: white;
+      font-size: 30px;
+    }
+  }
+}
+</style>
